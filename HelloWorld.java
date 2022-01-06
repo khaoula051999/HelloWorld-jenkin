bcf6915c315009dac447f5783834ff915b946ad4
Abouelfadl khaoula <81436839+khaoula051999@users.noreply.github.com>
2022-01-06 17:37:31 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-         System.out.println("Hello World khaoula");
+         System.out.println("Hello World khaoula 2eme commit");
     }
 }
